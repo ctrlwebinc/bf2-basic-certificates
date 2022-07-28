11863f94a0df93a00f1515435627a61be1e1490c
Tahina <to.tahina@gmail.com>
2022-07-28 21:31:50 +0300

    Install dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,74 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0c722f158032db0bd4dcb31afa4d2479fbeaab5d',
+    'name' => 'ctrlwebinc/bf2-certificates',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.12.0',
+      'version' => '1.12.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+    ),
+    'ctrlwebinc/bf2-certificates' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0c722f158032db0bd4dcb31afa4d2479fbeaab5d',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'setasign/fpdf' => 
+    array (
+      'pretty_version' => '1.8.4',
+      'version' => '1.8.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b0ddd9c5b98ced8230ef38534f6f3c17308a7974',
+    ),
+    'setasign/fpdi' => 
+    array (
+      'pretty_version' => 'v2.3.6',
+      'version' => '2.3.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6231e315f73e4f62d72b73f3d6d78ff0eed93c31',
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'tecnickcom/tcpdf' => 
+    array (
+      'pretty_version' => '6.4.4',
+      'version' => '6.4.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '42cd0f9786af7e5db4fcedaa66f717b0d0032320',
+    ),
+  ),
+);
